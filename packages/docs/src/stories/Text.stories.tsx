d832ca1399c610879ccfd00c753979e3b288ef5a
João Guilherme Da Rocha <joaoguibc@gmail.com>
2022-10-27 19:59:45 +0000

    Feat: Created and documented a new component: Heading

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -16,5 +16,25 @@ export const CustomTag: StoryObj<TextProps> = {
   args: {
     children: 'Strong text',
     as: 'strong',
+  },import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@joaoguibc-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste laborum optio debitis autem labore numquam esse molestiae aliquid, repudiandae accusantium omnis nisi, voluptatibus aliquam modi illum! Quisquam blanditiis aliquam dolore!',
   },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
+
 }
