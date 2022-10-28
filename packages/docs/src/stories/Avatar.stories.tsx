5400139d5e6591ee089b588d6ad99eff799e0b28
joaoguibc <joaoguibc@gmail.com>
2022-10-27 23:29:32 -0300

    Feat: Improved component control on Storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/joaoguibc.png',
     alt: 'João Guilherme Da Rocha',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
